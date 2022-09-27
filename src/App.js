0c85efd9b6a8472c09cd2cbc351b3efb0163df08
AmairaniCR <rosalesg155@gmail.com>
2022-09-26 22:15:41 -0500

    prueba consumir datos

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "./styles.css";
+import RandomUser from "./componentes/RandomUser";
+
+function App() {
+  return (
+    <div className="App">
+      <RandomUser />
+    </div>
+  );
+}
+
+export default App;
